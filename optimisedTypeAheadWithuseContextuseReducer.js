f7317d85c0096b24a81bd4f22b6e5cfaa58a5fe1
hardrock1997 <52746490+hardrock1997@users.noreply.github.com>
2024-01-27 13:34:07 +0530

    Update optimisedTypeAheadWithuseContextuseReducer.js

--- optimisedTypeAheadWithuseContextuseReducer.js
+++ optimisedTypeAheadWithuseContextuseReducer.js
@@ -1,1 +1,11 @@
+CodeSandbox link:
+
 https://codesandbox.io/p/sandbox/typeahead-forked-qdhvwr?file=%2Fsrc%2FApp.js%3A11%2C1
+
+/*
+useDebounce is used to debounce the search bar typed value.
+useReducer is used inside the useFetch custom hook to handle the loading, error and data.
+useFetch is the custom hook for fetching products data.
+productsContext is used to pass data in Search and Products sibling components.
+*/
+
